63b3b97bd46c550a107d5620bfc446f78e234bcb
Nikita Hismatov <hismatovns@gmail.com>
2021-04-04 02:49:40 +0500

    черновой вариант

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+extern crate rand;
+
+use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+use rand::Rng;
+
+// fn main() {
+//     let mut rng = rand::thread_rng();
+
+//     let n1: u8 = rng.gen();
+//     let n2: u16 = rng.gen();
+
+// TODO: добавить размышления
+
 fn main() {
-    println!("Hello, world!");
+    const PHILOSOPHERS_NUMBER: usize = 5;
+    const MAX_SECONDS_TO_EAT: u64 = 1;
+    const MAX_SECONDS_TO_THINK: u64 = 1;
+
+    let mut children = Vec::with_capacity(PHILOSOPHERS_NUMBER);
+
+    let forks_taken = Arc::new(Mutex::new(HashSet::with_capacity(PHILOSOPHERS_NUMBER)));
+
+    for n in 0..PHILOSOPHERS_NUMBER {
+        let fork_arc = forks_taken.clone();
+        children.push(thread::spawn(move || -> ! {
+            let current_fork_number = n;
+            let next_fork_number = (n + 1) % PHILOSOPHERS_NUMBER;
+
+            let fork_to_take_first = min(current_fork_number, next_fork_number);
+            let fork_to_take_last = max(current_fork_number, next_fork_number);
+            
+            let mut rng = rand::thread_rng();
+
+            loop { // цикл раздумий и еды...
+                println!("Philosopher#{} is thinking...", n);
+                thread::sleep(Duration::from_secs(rng.gen_range(0, MAX_SECONDS_TO_THINK) as u64));
+
+                // блокируем сет вилок, чтобы узнать, а можно ли перекусить
+                let mut forks_taken = fork_arc.lock().unwrap();
+                if !forks_taken.contains(&fork_to_take_first)
+                    && !forks_taken.contains(&fork_to_take_last)
+                {
+                    // да, можно кушать!
+                    forks_taken.insert(fork_to_take_first);
+                    println!("Philosopher#{} took fork#{}", n, fork_to_take_first);
+
+                    forks_taken.insert(fork_to_take_last);
+                    println!("Philosopher#{} took fork#{}", n, fork_to_take_last);
+
+                    // освобождаем блокировку сета вилок
+                    drop(forks_taken);
+
+                    // едим...
+                    println!("Philosopher#{} is eating.", n);
+                    thread::sleep(Duration::from_secs(rng.gen_range(0, MAX_SECONDS_TO_EAT) as u64));
+
+                    // снова блокируем сет вилок
+                    forks_taken = fork_arc.lock().unwrap();
+                    forks_taken.remove(&fork_to_take_last);
+                    forks_taken.remove(&fork_to_take_first);
+                }
+                thread::sleep(Duration::from_secs(1));
+            }
+        }));
+    }
+
+    for child in children {
+        let _ = child.join();
+    }
 }
